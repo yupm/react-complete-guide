c118e9c9e080a7a6186902b3c089ac9108c53dfa
Milton <35495603+yupm@users.noreply.github.com>
2018-08-28 23:06:10 +0800

    Lecture 128

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -13,6 +13,7 @@ const controls = [
 
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
+        <p>CurrentPrice: <strong>{props.price.toFixed(2)}</strong></p>
         { controls.map(ctrl => (
                 <BuildControl 
                 key={ctrl.label} 
@@ -22,6 +23,10 @@ const buildControls = (props) => (
                 disabled = {props.disabled[ctrl.type]}
                 />
         ))}
+        <button className={classes.OrderButton} 
+                disabled={!props.purchasable}
+                onClick={props.ordered}
+        >ORDER NOW</button>
     </div>
 );
 
--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -7,7 +7,6 @@ const burger = (props) => {
     let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i)=>{
-                console.log("The key is", igKey);
                 return <BurgerIngredient key={igKey + i} type={igKey}/>;
             });
         })
@@ -15,8 +14,6 @@ const burger = (props) => {
             return arr.concat(el)
         },[]);
 
-
-    console.log(transformedIngredients);
 
     if(transformedIngredients.length === 0){
         transformedIngredients = <p>Please start adding ingredients!</p>
